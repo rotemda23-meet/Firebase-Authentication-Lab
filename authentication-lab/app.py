7b7d1d440adc6131ee3d0a93f91ff132a51bf1d6
rotemda23-meet <109904555+rotemda23-meet@users.noreply.github.com>
2022-08-01 15:47:37 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,31 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
+
+
 config= {"apiKey": "AIzaSyB29sOtmoLJlQAsaun0ntE8xKKwW2Dwz_8",
   "authDomain": "rotem-and-more.firebaseapp.com",
   "projectId": "rotem-and-more",
   "storageBucket": "rotem-and-more.appspot.com",
   "messagingSenderId": "457525107556",
   "appId": "1:457525107556:web:cafbe24bf75099dab97871",
-  "measurementId": "G-BEHV39NH3Q"
+  "measurementId": "G-BEHV39NH3Q",
+  "databaseURL": ""
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('signin.html')
     else:
         password=request.form["password"]
         email=request.form["email"]
         try:
-            login_session['user'] = auth.sign_in_user_with_password_and_email(email, password)
+            login_session['user'] = auth.sign_in_user_with_email_and_password(email, password)
             return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
@@ -36,12 +38,12 @@ def signin():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('signup.html')
     else:
         password=request.form["password"]
         email=request.form["email"]
         try:
-            login_session['user'] = auth.create_user_with_password_and_email(email, password)
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
             return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
